cee7da4471e9b99553a19c08a3c9870b81cd60e8
Snir Holland <sniro84@hotmail.com>
2019-07-13 16:23:34 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,64 +3,32 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-const MAX_STUDENTS_PER_PAGE = 10;
+const MAX_ITEMS_PER_PAGE = 10;
 const studentsList = document.querySelectorAll('li.student-item');
 
+
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+   This function recieves a list and a page number
+   and shows all of the items that belong to the page.
 ***/
-
 function showPage(list, page)
 {
-   const startIndex = MAX_STUDENTS_PER_PAGE * (page - 1);
-   const endIndex = MAX_STUDENTS_PER_PAGE * page - 1;
+   const startIndex = MAX_ITEMS_PER_PAGE * (page - 1);  // index of first element.
+   const endIndex = MAX_ITEMS_PER_PAGE * page - 1;      // index of last element.
 
     for(let i=0; i<list.length; i++)
     {
-      if (i>=startIndex  &&  i<=endIndex)
-            list[i].style.display = '';
-      else
+      if (i>=startIndex  &&  i<=endIndex)      // item belongs to the page --> show it .
+            list[i].style.display = '';    
+      else                                     // item doesn't belong to the page --> hide it.
             list[i].style.display = 'none';     
     }              
 }
 
-
-
-
-
-
-
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+   This function recieves a list and adds  
    functionality to the pagination buttons.
 ***/
-
 function appendPageLinks(list)
 {
    // get the required number of pages for the list items
@@ -85,48 +53,62 @@ function appendPageLinks(list)
       const li = document.createElement('li');
       ul.appendChild(li);
       
-      // create 'a' element and adjust its href property and text
-      const a = document.createElement('a');
-      li.appendChild(a);
-      a.href = '#';
-      a.text = i;
-
+      // create a link 
+      const link = document.createElement('a');
+      li.appendChild(link);
+      link.href = '#';
+      link.text = i;
    }
    
+   // make the first link active
    const firstLink = document.querySelector('div.pagination > ul > li > a');
    firstLink.className = "active";
    
-   const anchorTags = document.querySelectorAll('div.pagination > ul > li > a');
-   for(let i = 0;  i<=anchorTags;  i++)
+   // add event listeners to all links 
+   const links = document.querySelectorAll('div.pagination > ul > li > a');
+   for(let i = 0;  i<=links.length;  i++)
    {
-         anchorTags[i].addEventListener('click', (event) => {
-            showPage(studentsList , i+1);
-            setAction(anchorTags, event);
+         links[i].addEventListener('click', (event) => {
+            showPage(studentsList , i+1);       // show the items that belong to the page.
+            const targetLink = event.target; 
+            activateLink(links, targetLink);    // activate target link and deactivate the rest of them.
          });
    }
 
 }
 
-// helper function
+/*** 
+   This helper function recieves a list and returns  
+   the number of the pages that are going to be required
+   in order to display all of the elements in that list.
+***/
 function getRequiredPages(list)
 {
-   if (list.length % MAX_STUDENTS_PER_PAGE === 0)
-      return list.length / MAX_STUDENTS_PER_PAGE;
-   else
-      return list.length / MAX_STUDENTS_PER_PAGE + 1;
+   // assuming the list length divided by the max items per page has no remainder. 
+   let result = list.length / MAX_ITEMS_PER_PAGE;
+
+   // in case the list length divided by the max items per page has a remainder.   
+   if (list.length % MAX_ITEMS_PER_PAGE !== 0)
+      result = parseInt(Math.ceil(result));         // updating result.
+
+   // result now stores the correct value.   
+   return result;   
 }
 
-// another hekper function
-function setAction(anchors, event)
+/*** 
+   This helper function recieves an array of links and a target link.
+   the function activates the target link and deactivates the rest
+   of the links in the array.  
+***/
+function activateLink(links, targetLink)
 {
-  for(let i=0; i<anchors.length; i++)
-      anchors[i].classList.remove('active');
-  const currentAnchor = event.target; 
-  currentAnchor.classList.add('active');
-  
+  // deactivation of all the links. 
+  for(let i=0; i<links.length; i++)
+      links[i].classList.remove('active');
+
+  // activation of target link.    
+  targetLink.classList.add('active');
 }
 
 showPage(studentsList,1);
 appendPageLinks(studentsList);
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
