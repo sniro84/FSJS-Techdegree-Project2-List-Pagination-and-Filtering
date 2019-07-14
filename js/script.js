5665fa6b8ba4373e13964745e1505fd619344dd2
Snir Holland <sniro84@hotmail.com>
2019-07-14 17:03:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -66,16 +66,30 @@ function appendPageLinks(list)
    
    // add event listeners to all links 
    const links = document.querySelectorAll('div.pagination > ul > li > a');
-   for(let i = 0;  i<=links.length;  i++)
+   for(let i = 0;  i<links.length;  i++)
    {
          links[i].addEventListener('click', (event) => {
-            showPage(studentsList , i+1);       // show the items that belong to the page.
+            showPage(list , i+1);       // show the items that belong to the page.
             const targetLink = event.target; 
             activateLink(links, targetLink);    // activate target link and deactivate the rest of them.
          });
    }
 
 }
+
+function clearPage()
+{
+   for(let i=0; i<studentsList.length; i++)
+      studentsList[i].style.display = 'none';
+}
+
+function deletePageLinks()
+{
+   const containerDiv = document.querySelector('div.pagination');
+   containerDiv.remove();
+}
+
+
 
 /*** 
    This helper function recieves a list and returns  
@@ -110,8 +124,114 @@ function activateLink(links, targetLink)
   targetLink.classList.add('active');
 }
 
+function appendSearch()
+{
+   const ul = document.querySelector('ul.student-list');
+   const parentDiv = ul.parentNode;
+   const searchDiv = document.createElement('div');
+   searchDiv.className = "page-header";
+   searchDiv.className = "student-search";
+   parentDiv.insertBefore(searchDiv, ul);
+
+   const input = document.createElement('input');
+   input.placeholder = "Search for students...";
+   searchDiv.appendChild(input);
+
+   const searchButton = document.createElement('button');
+   searchButton.textContent = "Search";
+   searchDiv.appendChild(searchButton);
+
+   searchDiv.align = "right";
+
+   const h5 = document.createElement('h5');
+      searchDiv.appendChild(h5);  
+      let messageToUser = "";
+      h5.innerText = messageToUser;
+
+   searchButton.addEventListener('click', (event) => {
+      event.preventDefault();
+   
+      let results = 0;
+      
+      if (input.value.length !== 0)
+         results = searchForItem(input, studentsList);
+      
+      if (results === 0)
+      {
+         messageToUser = "No results were found.";
+         h5.innerText = messageToUser;
+      }
+      else
+      {
+         messageToUser = "" + results + " results were found";
+         h5.innerText = messageToUser;
+      }
+            
+ 
+    });
+  
+}
+
+function searchForItem(searchInput, items)
+{
+   const matchItems = [];
+   for(let i=0; i<items.length; i++)
+   {
+
+      const inputString = searchInput.value.toLowerCase();
+
+      const h3 = items[i].querySelector('div.student-details > h3');
+      const itemNameString = h3.innerText.toLowerCase(); 
+      
+      const emptyString = (searchInput.value.length === 0);
+      
+      const containsInput = (itemNameString.search(inputString) !== -1);    
+
+
+      if (!emptyString && containsInput)
+      {
+         matchItems.push(items[i]);
+         console.log("found a match" + "in name : " + itemNameString);
+      }
+                
+   }
+   console.log(matchItems);
+
+   try
+   {
+      clearPage();
+      deletePageLinks();
+   }
+   catch(error) 
+   {
+       console.log("trying to delete links (page numbers) that doesn't exist." + 
+                    "It happend because the last search resulted in 0 matches"); 
+   } 
+   
+
+   if (matchItems.length !== 0)
+   {
+      showPage(matchItems,1);
+      appendPageLinks(matchItems);
+   }
+   
+   return matchItems.length;
+}
+
+
+
+function deletePageLinks() 
+{
+   const ul = document.querySelector('div.pagination');
+      ul.remove();
+}
+
+
 // show first page by default.
 showPage(studentsList,1);
 
 // append the page links to the screen.
 appendPageLinks(studentsList);
+
+
+appendSearch();
